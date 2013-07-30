68a52cc83f137ef0f7c8f7a10ef935d404c730bb
Mayas <mayas_91@hotmail.com>
2013-07-30 11:06:46 +0200

    avoiding the zero date case after DateTime formatting

--- tools/Utility.php
+++ tools/Utility.php
@@ -6,7 +6,9 @@ class Utility{
 
 	public static function getNoZeroDate($dateString){
 		// This method makes sure that no 0-date is given from Prestashop, because SQLServer does not allow it
-		if($dateString == '0000-00-00 00:00:00'){
+		// -0001-11-30 00:00:00 is returned by DateTime::format('0000-00-00 00:00:00')
+		if($dateString == '0000-00-00 00:00:00'
+			|| $dateString == '-0001-11-30 00:00:00' ){
 			return NULL; // This date is the Gestimum default "no date" date
 		}
 		return $dateString;
