853431dc3a8c0f63c80ed0a09540a65e57ed5bcd
mayas-pc-bureau <mayas_91@hotmail.com>
2013-09-03 17:25:42 +0200

    corrected the operation, adding one systematically

--- tools/scripts/js/checks.js
+++ tools/scripts/js/checks.js
@@ -1,6 +1,7 @@
 
 function checkData(form, entity){
 	var numberOfEntities = form.to.value - form.from.value;
+	numberOfEntities += 1;
 	var stringOfNumberOfEntities = numberOfEntities.toString();
 	return confirm("Vous êtes sur le point de mettre à jour " + stringOfNumberOfEntities + " " + entity + "(s)");
 }
